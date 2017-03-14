38635346f342c92159e52b12ba8286e9bd71720b
alexanderArsenault <rsenaultc@gmail.com>
2017-03-14 14:54:05 -0400

    Cleaned template

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('alex is right');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
